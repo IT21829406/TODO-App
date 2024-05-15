4da9c697596875d65d366e10d87a9834a5031d22
Sutharsan <it21829406@my.sliit.lk>
2024-05-15 22:09:45 +0530

    db update

--- back_end/config/db.js
+++ back_end/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/todo-list-app', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/todo-list-app', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(error => console.error('Error connecting to MongoDB:', error));
